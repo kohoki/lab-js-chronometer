76ec34c3e7046eeb16c0e4b773ef3bbb718c4c38
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-11 14:50:37 +0200

    finish iteratio 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -43,6 +43,6 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
